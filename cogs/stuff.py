d99757980a633c03d0d0cd2a97903ba921d8429e
Sam Mitchell <git@wont.cc>
2020-09-04 19:19:50 +0100

    Add image command

--- cogs/stuff.py
+++ cogs/stuff.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
+from configparser import ConfigParser
 from udpy import UrbanClient
+from google_images_search import GoogleImagesSearch
 
 class Stuff(commands.Cog):
     def __init__(self, bot):
@@ -16,6 +18,28 @@ class Stuff(commands.Cog):
                 await ctx.send(m.replace('[','').replace(']',''))
             else:
                 await ctx.send(f'No definition found for {word}')
+
+    @commands.command(aliases=('img',))
+    async def image(self, ctx, *args):
+        if args:
+            search = {
+                'q': ' '.join(args),
+                'num': 1,
+                'safe': 'off',
+                }
+
+            # TODO: move to only loading keys on cog load, this is bad
+            config = ConfigParser()
+            config.read('bot.cfg')
+            if config['google']['api_key'] and config['google']['engine_id']:
+                key = config['google']['api_key']
+                cx = config['google']['engine_id']
+            else:
+                return
+
+            gis = GoogleImagesSearch(key, cx)
+            gis.search(search_params=search)
+            await ctx.send(gis.results()[0]._url)
 
 def setup(bot):
     bot.add_cog(Stuff(bot))
