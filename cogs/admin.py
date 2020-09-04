f42351ba9358a1c212f29e90378da078d20de0bd
Sam Mitchell <git@wont.cc>
2020-09-04 17:19:09 +0100

    Fix elif to if

--- cogs/admin.py
+++ cogs/admin.py
@@ -10,7 +10,8 @@ class Admin(commands.Cog):
         if ctx.author.id == 651651128521523210:
             await ctx.send('F O R F R E E')
             sleep(1) 
-        elif ctx.author.permissions_in(ctx.channel).manage_messages and args[0].isdigit():
+
+        if ctx.author.permissions_in(ctx.channel).manage_messages and args[0].isdigit():
             async for message in ctx.channel.history(limit=int(args[0])+1):
                 await message.delete()
 
