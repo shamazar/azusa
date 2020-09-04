18645002de283cb048c3573a6fe29e095b110857
Sam Mitchell <git@wont.cc>
2020-09-04 17:12:52 +0100

    Add prune command

---
+++ cogs/admin.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+from time import sleep
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def prune(self, ctx, *args):
+        if ctx.author.id == 651651128521523210:
+            await ctx.send('F O R F R E E')
+            sleep(1) 
+        elif ctx.author.permissions_in(ctx.channel).manage_messages and args[0].isdigit():
+            async for message in ctx.channel.history(limit=int(args[0])+1):
+                await message.delete()
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
--- cogs/test.py
+++ cogs/test.py
@@ -6,7 +6,10 @@ class Test(commands.Cog):
 
     @commands.command()
     async def test(self, ctx, *args):
-        await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+        if self.bot.is_owner(ct.author):
+            #await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+            perms = ctx.author.permissions_in(ctx.channel).manage_messages
+            await ctx.send(str(perms))
 
 def setup(bot):
     bot.add_cog(Test(bot))
