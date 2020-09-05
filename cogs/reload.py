f3af4a7a81e5f6014a2eb1c933a1bb6d8e6e16b5
Sam Mitchell <git@wont.cc>
2020-09-05 17:56:06 +0100

    Change to is_owner decorators

--- cogs/reload.py
+++ cogs/reload.py
@@ -7,35 +7,36 @@ class Reload(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.is_owner()
     async def reload(self, ctx, *args):
-        if self.bot.is_owner(ctx.author):
-            if not args:
-                for cog in self.bot.cogs.keys():
-                    self.bot.reload_extension(f'cogs.{cog.lower()}')
-                await ctx.send(f'{len(self.bot.cogs)} cogs reloaded.')
+        if not args:
+            for cog in self.bot.cogs.keys():
+                self.bot.reload_extension(f'cogs.{cog.lower()}')
+            await ctx.send(f'{len(self.bot.cogs)} cogs reloaded.')
+            self.bot.config = reload_cfg('bot.cfg')
+        elif len(args) == 1:
+            cog = args[0]
+            if cog == "cfg":
                 self.bot.config = reload_cfg('bot.cfg')
-            elif len(args) == 1:
-                cog = args[0]
-                if cog == "cfg":
-                    self.bot.config = reload_cfg('bot.cfg')
-                    await ctx.send('Config reloaded')
-                elif cog in self.bot.cogs.keys():
-                    await ctx.send(f'Reloading {cog}...')
-                    self.bot.reload_extension(f'cogs.{cog.lower()}')
-                else:
-                    await ctx.send(f'{cog} not recognised.')
-
+                await ctx.send('Config reloaded')
+            elif cog in self.bot.cogs.keys():
+                await ctx.send(f'Reloading {cog}...')
+                self.bot.reload_extension(f'cogs.{cog.lower()}')
             else:
-                await ctx.send('Unrecognised arguments, please specify a cog.')
+                await ctx.send(f'{cog} not recognised.')
+
+        else:
+            await ctx.send('Unrecognised arguments, please specify a cog.')
     
     @commands.command()
+    @commands.is_owner()
     async def listcogs(self, ctx):
-        if self.bot.is_owner(ctx.author):
-            await ctx.send(', '.join(self.bot.cogs.keys()))
+        await ctx.send(', '.join(self.bot.cogs.keys()))
 
     @commands.command()
+    @commands.is_owner()
     async def git(self, ctx, *args):
-        if len(args) == 1 and self.bot.is_owner(ctx.author):
+        if len(args) == 1:
             if args[0] == 'pull':
                 res = subprocess.check_output(['git', 'pull']).decode('utf-8')
                 await ctx.send('```' + res + '```')
