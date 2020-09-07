d1984c3634aa4d296b45465782ddbd7890654822
Sam Mitchell <git@wont.cc>
2020-09-07 19:00:51 +0100

    F O R F R E E

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,16 +7,16 @@ class Admin(commands.Cog):
 
     @commands.command()
     async def prune(self, ctx, *args):
-        if ctx.author.id == 651651128521523210:
-            await ctx.send('F O R F R E E')
-            sleep(1) 
-
         if ctx.author.permissions_in(ctx.channel).manage_messages and args[0].isdigit():
             # smol limit because discord rate limits
             limit = 10
             if limit > int(args[0]): limit = args[0]
             async for message in ctx.channel.history(limit=int(args[0])+1):
                 await message.delete()
+
+        if ctx.author.id == 651651128521523210:
+            await ctx.send('F O R F R E E')
+            sleep(1) 
 
 def setup(bot):
     bot.add_cog(Admin(bot))
